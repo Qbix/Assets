fe194f9e922efc4a74222e955f2f6ed326a2cb0a
Greg Magarshak <greg@qbix.com>
2024-11-13 00:58:34 -0500

    Fixed installer scripts when Assets/stripe configs aren't set

--- scripts/Assets/1.0.171-Streams.mysql.php
+++ scripts/Assets/1.0.171-Streams.mysql.php
@@ -1,12 +1,16 @@
 <?php
 
 function Assets_1_0_171_Streams_mysql() {
-	$rows = Assets_Customer::select()->where(['payments' => 'stripe'])->fetchDbRows();
-	foreach ($rows as $row) {
-		if (empty($row->hash)) {
-			$row->hash = Assets_Customer::getHash();
-			$row->save();
+	try {
+		$rows = Assets_Customer::select()->where(['payments' => 'stripe'])->fetchDbRows();
+		foreach ($rows as $row) {
+			if (empty($row->hash)) {
+				$row->hash = Assets_Customer::getHash();
+				$row->save();
+			}
 		}
+	} catch (Exception $e) {
+		// just ignore the error, config is probably not set
 	}
 }
 
