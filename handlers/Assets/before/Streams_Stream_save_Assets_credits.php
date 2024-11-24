49f05f007cab78979bf1792c55432e80ec1c6c88
Greg Magarshak <greg@qbix.com>
2024-11-24 03:16:47 -0500

    Renamed file for hook

--- handlers/Assets/before/Streams_Stream_save_Assets_credits.php
+++ handlers/Assets/before/Streams_Stream_save_Assets_credits.php
@@ -2,10 +2,11 @@
 
 function Assets_before_Streams_Stream_save_Assets_credits($params)
 {
-    $row = $params['stream'];
+    $stream = $params['stream'];
     $amount = $stream->getAttribute('amount', null);
     $peak = $stream->getAttribute('peak', 0);
     if (isset($amount)) {
         $stream->setAttribute('peak', max($peak, $amount));
+        $params['modifiedFields']['attributes'] = $stream->attributes;
     }
 }
